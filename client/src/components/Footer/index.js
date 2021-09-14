1a6ba034b067fef340b5799838246a96787e4acd
damatgre <gregdamato2@gmail.com>
2021-09-14 19:49:21 -0400

    header and footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { GiFoodTruck } from 'react-icons/gi';
+
+
+function Footer() {
+    return (
+
+        <footer className="text-center text-lg-start bg-dark fixed-bottom">
+            <section className="d-flex justify-content-center  p-4 border-bottom">
+                <div>
+
+                    <p href="https://github.com/JillianA328/be-our-guest" className="me-4 text-white fs-1">
+                        <GiFoodTruck />
+                    </p>
+
+                </div>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
