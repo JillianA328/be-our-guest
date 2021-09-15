1eb33e06fcaffbf85ae0dee84df683b266171b50
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-09-14 20:31:00 -0400

    include typeDefs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/be-our-guest', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+// Import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// Create our typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        reviews: [Review]
+    }
+
+    type Review {
+        _id: ID
+        reviewText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        reviews(username: String): [Review]
+        review(_id: ID!): Review
+    }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addReview(thoughtText: String!): Review
+        addReaction(thoughtId: ID!, reactionBody: String!): Review
+    }
+`;
+
+// Export the typeDefs
+module.exports = typeDefs;
