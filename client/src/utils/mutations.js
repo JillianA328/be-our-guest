dcc528a2e4d2a0a950bc55b65ef58673a933163c
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-09-15 20:48:49 -0400

    added ADD REVIEW MUTATION LAYout

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,15 @@ mutation addUser($username: String!, $email: String!, $password: String!){
     }
 }
 `;
+
+export const ADD_REVIEW = gql`
+mutation addUser($username: String!, $email: String!, $password: String!){
+    addUser(username: $username, email: $email, password: $password){
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
