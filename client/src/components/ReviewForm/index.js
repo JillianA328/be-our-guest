bb2d83b79e4959afbf746bce419328fba43c7bf9
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-09-15 21:43:39 -0400

    successfully added ability to post reviews

--- client/src/components/ReviewForm/index.js
+++ client/src/components/ReviewForm/index.js
@@ -1,9 +1,36 @@
 import React, { useState } from 'react';
+import { useMutation } from "@apollo/client";
+import { ADD_REVIEW} from "../../utils/mutations";
+import { QUERY_REVIEWS, QUERY_ME } from "../../utils/queries";
 
 const ReviewForm = () => {
 
     const [reviewText, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+
+    const [addReview, { error }] = useMutation(ADD_REVIEW, {
+        update(cache, { data: { addReview } }) {
+          try {
+            // read what's currently in cache
+            const { reviews } = cache.readQuery({ query: QUERY_REVIEWS });
+    
+            // prepend the newest thought to the front of the array
+            cache.writeQuery({
+              query: QUERY_REVIEWS,
+              data: { reviews: [addReview, ...reviews] },
+            });
+          } catch (e) {
+            console.log(e);
+          }
+    
+          // update me object's cache, appending a new thought to the end of the list
+          const { me } = cache.readQuery({ query: QUERY_ME });
+          cache.writeQuery({
+              query: QUERY_ME,
+              data: { me: {...me, reviews: [...me.reviews, addReview] } }
+          });
+        }
+      });
 
     const handleChange = event => {
         if (event.target.value.length <= 280) {
@@ -12,38 +39,21 @@ const ReviewForm = () => {
         }
     };
 
-    const handleFormSubmit = async event => {
+    const handleFormSubmit = async (event) => {
         event.preventDefault();
-        setText('');
-        setCharacterCount(0);
-    }
-
-    const handleAddReview = async (bookId) => {
-        // find the book in `searchedBooks` state by the matching id
-        const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-    
-        // get token
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-    
-        if (!token) {
-          return false;
-        }
     
         try {
-      
-          // updated to use new mutation
-          const { data } = await saveBook({
-            variables: { bookData: { ...bookToSave } },
+          // add thought to database
+          await addReview({
+            variables: { reviewText },
           });
-    
-          console.log("data:", data);
-    
-          // if book successfully saves to user's account, save book id to state
-          setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-        } catch (err) {
-          console.error(err);
+          setText("");
+          setCharacterCount(0);
+        } catch (e) {
+          console.log(e);
         }
       };
+    
     
     return (
         <div>
