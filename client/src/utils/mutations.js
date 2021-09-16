dd240af4faeeaf196f16bb77168c8ffbf567fffa
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-09-15 20:57:40 -0400

    added save book mutation front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,14 +24,18 @@ mutation addUser($username: String!, $email: String!, $password: String!){
 }
 `;
 
-export const ADD_REVIEW = gql`
-mutation addUser($username: String!, $email: String!, $password: String!){
-    addUser(username: $username, email: $email, password: $password){
-        token
-        user {
-            _id
-            username
-        }
+export const SAVE_BOOK = gql`
+  mutation saveBook($reviewData: ReviewInput!) {
+    saveReview(reviewData: $reviewData) {
+      _id
+      username
+      email
+      savedReviews {
+        reviewId
+        reviewText
+        createdAt
+        reactions
+      }
     }
-}
+  }
 `;
