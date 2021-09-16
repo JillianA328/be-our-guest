ac8955a6cf3b30cb388266c4ab9c5bd0718de0b2
JillianA328 <jpamelung@gmail.com>
2021-09-16 15:58:00 -0400

    working on API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,14 +2,6 @@ import React from "react";
 const Documenu = require('documenu');
 Documenu.configure('bc07f64299960a8b13f9a3d3e07ebf52');
 
-// make a search to documenu restaurant api
-
-
-// export const searchRestuaraunts = (query) => {
-//     return fetch(`https://api.documenu.com/v2/restaurant/4072702673999819?key=bc07f64299960a8b13f9a3d3e07ebf52`);
-// };
-
-
 export default class DocuMenu extends React.Component {
     constructor(props) {
         super(props);
@@ -51,8 +43,8 @@ export default class DocuMenu extends React.Component {
             return (
                 <ul>
                     {items.map(item => (
-                        <li key={item.id}>
-                            {item.name}
+                        <li key={restaurant.id}>
+                            {restaurant.name}
                         </li>
                     ))}
                 </ul>
