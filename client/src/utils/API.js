812c2a4085bf2fff46dce1dfb24a35034f765f12
JillianA328 <jpamelung@gmail.com>
2021-09-15 21:21:12 -0400

    added auth.js file for api

---
+++ client/src/utils/API.js
@@ -0,0 +1,7 @@
+
+
+// make a search to documenu restaurant api
+
+export const searchRestuaraunts = (query) => {
+    return fetch(`https://api.documenu.com/v2/restaurant/4072702673999819?key=bc07f64299960a8b13f9a3d3e07ebf52`);
+};
